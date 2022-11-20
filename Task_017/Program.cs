34130e17d872abaa11159c725a98907c1b969ace
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-20 22:24:00 +0300

    Initial workshop3

---
+++ Task_017/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+//точки (X и Y), 
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+//  в которой находится эта точка.
+
+var x = Convert.ToInt32(Console.ReadLine());
+var y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine('1');
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine('2');
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine('3');
+}
+else
+{
+    Console.WriteLine('4');
+}
---
+++ Task_021/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между ними 
+// в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите значение x1: ");
+var x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение : ");
+var y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение: ");
+var x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение: ");
+var y2 = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+Console.WriteLine(result);
+// dist = Math.Round(dist, 2, MidpointRounding.ToZero); 
